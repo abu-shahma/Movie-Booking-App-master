64c34d5457283a2762bcc50c9652b88a05489587
abu-shahma <abu.shahma1155@gmail.com>
2024-07-21 23:01:49 +0530

    FULL

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './screens/Home/Home';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
